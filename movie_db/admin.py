a21649a152a8904680024a8c7b80b501999a9732
msk007 <krawiec515@gmail.com>
2015-10-06 22:37:16 -0400

    made classes in admin

--- movie_db/admin.py
+++ movie_db/admin.py
@@ -5,9 +5,19 @@ from .models import Movie, User_id, Rating
 
 
 
-# Register your models here.
+
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display =['user_id']
 
 
 class MovieAdmin(admin.ModelAdmin):
+    list_display =['movie_id']
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display =['user_id','movie_id','rating']
+
+# Register your models here.
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(User_id, UserAdmin)
+admin.site.register(Rating, RatingAdmin)
