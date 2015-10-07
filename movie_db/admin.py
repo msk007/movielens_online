d512198cdba5e8bf230281d8aedd2018e86bbcc0
msk007 <krawiec515@gmail.com>
2015-10-06 22:29:36 -0400

    update

--- movie_db/admin.py
+++ movie_db/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from .models import Movie, User_id, Rating
 
 
 
 
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+class MovieAdmin(admin.ModelAdmin):
--- movie_db/models.py
+++ movie_db/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
+class User_id (models.Model):
+    user_id=models
+
+
+class Movie(models.Model):
+    movie_id=models.CharField(max_length=200)
+
+class Rating(models.Model):
+    user_id = models.ForeignKey(User_id)
+    movie = models.ForeignKey(Movie)
+    rating = models.FloatField()
+
+    def__str__(self):
