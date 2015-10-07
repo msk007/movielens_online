2a94a81facc4a3d827ac98732066aa1cb7b02712
msk007 <krawiec515@gmail.com>
2015-10-06 21:43:45 -0400

    update

--- movie_db/admin.py
+++ movie_db/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Status, Favorite
 
-Class StatusAdmin(admin.ModelAdmin):
 
-admin.site.register(Status, StatusAdmin)
-admin.site.register(Favorite)
+
+
+
 # Register your models here.
--- movie_db/models.py
+++ movie_db/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
-class User(models.Model):
-
-class Movies(models.Model):
-
-class Ratings(model.Models):
